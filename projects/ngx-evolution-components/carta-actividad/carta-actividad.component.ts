015ed06fad2cc7d2490364ac3b28029b54ec1466
OJP98 <ojuarez_p@hotmail.com>
2024-08-02 16:39:40 -0600

    DEV-4008 DEV-4022 #see #comment Utilizando componente de avatar en carta actividad

--- projects/ngx-evolution-components/carta-actividad/carta-actividad.component.ts
+++ projects/ngx-evolution-components/carta-actividad/carta-actividad.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { IActividad } from '../shared/interfaces/actividad.interface';
 import { TagComponent } from '../tag/tag.component';
 import { MatIconModule } from '@angular/material/icon';
+import { AvatarComponent } from '../public-api';
 
 @Component({
   selector: 'evo-carta-actividad',
   standalone: true,
-  imports: [TagComponent, MatIconModule],
+  imports: [TagComponent, AvatarComponent, MatIconModule],
   templateUrl: './carta-actividad.component.html',
   styleUrls: ['./carta-actividad.component.scss', '../styles/output.scss'],
 })

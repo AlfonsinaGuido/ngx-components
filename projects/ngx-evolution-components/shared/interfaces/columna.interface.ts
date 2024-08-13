261f34988f5cb01fe3875d88955453d60fbbd1fe
OJP98 <ojuarez_p@hotmail.com>
2024-08-13 16:25:28 -0600

    DEV-4008 DEV-4022 #see #comment mejora en el layout y funcionalidad del componente de columna-tablero

--- projects/ngx-evolution-components/shared/interfaces/columna.interface.ts
+++ projects/ngx-evolution-components/shared/interfaces/columna.interface.ts
@@ -1,6 +1,7 @@
 import { IActividad } from './actividad.interface';
 
 export interface IColumna {
+  id: string | number;
   nombre: string;
-  actividades: IActividad;
+  actividades: IActividad[];
 }

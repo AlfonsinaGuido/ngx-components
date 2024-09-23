6daa9f9409ec92f0a3b1b30d97c55c42008f3ee6
Alfonsina Guido <aguido@aseinfo.com.sv>
2024-09-23 10:09:15 -0300

    DEV-4005 DEV-4184 #see #comment Actualizo IEmpleado.

--- projects/ngx-evolution-components/shared/interfaces/api/iempleado.interface.ts
+++ projects/ngx-evolution-components/shared/interfaces/api/iempleado.interface.ts
@@ -19,8 +19,23 @@ interface IFotografia {
 
 interface IPlaza {
   Puesto: IPuesto;
+  Unidad: IUnidad;
+  Compania: ICompania;
+  CentroTrabajo: ICentroTrabajo;
 }
 
 export interface IPuesto {
   Nombre: string;
+}
+
+interface IUnidad {
+  Descripcion: string;
+}
+
+interface ICompania {
+  Descripcion: string;
+}
+
+interface ICentroTrabajo {
+  Descripcion: string;
 }

c12106e1f8e39de2f74b77b3e0f622a8c73cad68
Alfonsina Guido <aguido@aseinfo.com.sv>
2024-11-20 10:16:03 -0300

    DEV-4005 DEV-4220 #see #comment Corrijo animación slideDown.

--- projects/ngx-evolution-components/shared/animations/animations.ts
+++ projects/ngx-evolution-components/shared/animations/animations.ts
@@ -2,10 +2,16 @@ import { trigger, transition, style, animate } from '@angular/animations';
 
 export const slideDown = trigger('slideDown', [
   transition(':enter', [
-    style({ height: '0', opacity: 0 }),
-    animate('500ms ease-out', style({ height: '*', opacity: 1 })),
+    style({ transform: 'translateY(-100%)', opacity: 0 }),
+    animate(
+      '500ms ease-out',
+      style({ transform: 'translateY(0)', opacity: 1 }),
+    ),
   ]),
   transition(':leave', [
-    animate('500ms ease-in', style({ height: '0', opacity: 0 })),
+    animate(
+      '500ms ease-in',
+      style({ transform: 'translateY(-100%)', opacity: 0 }),
+    ),
   ]),
 ]);

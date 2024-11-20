28b9601b3cb8cd5eaf13b48332bb1d1120374858
Alfonsina Guido <aguido@aseinfo.com.sv>
2024-11-20 15:07:34 -0300

    DEV-4005 DEV-4220 #see #comment Configura poder deshabilitar la animación del banner.

--- projects/ngx-evolution-components/shared/animations/animations.ts
+++ projects/ngx-evolution-components/shared/animations/animations.ts
@@ -1,14 +1,27 @@
-import { trigger, transition, style, animate } from '@angular/animations';
+import {
+  trigger,
+  transition,
+  style,
+  animate,
+  state,
+} from '@angular/animations';
 
 export const slideDown = trigger('slideDown', [
-  transition(':enter', [
+  // State para cuando no se quiere animación
+  state('false', style({})),
+  state('true', style({})), // Estado final animado
+
+  // Transición para mostrar el elemento con animación
+  transition('void => true', [
     style({ transform: 'translateY(-100%)', opacity: 0 }),
     animate(
       '500ms ease-out',
       style({ transform: 'translateY(0)', opacity: 1 }),
     ),
   ]),
-  transition(':leave', [
+
+  // Transición para ocultar el elemento con animación
+  transition('true => void', [
     animate(
       '500ms ease-in',
       style({ transform: 'translateY(-100%)', opacity: 0 }),

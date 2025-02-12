88364a7b7649fd3d32e251692f6d9b7ef6d1ee7e
Alfonsina Guido <aguido@aseinfo.com.sv>
2025-02-12 12:13:07 -0300

    Actualizo paquete de storybook.

--- storybook-static/sb-addons/interactions-10/manager-bundle.js
+++ storybook-static/sb-addons/interactions-10/manager-bundle.js
@@ -13115,14 +13115,16 @@ In order to be iterable, non-array objects must have a [Symbol.iterator]() metho
             { style: { color: n.base } },
             "[\u2026]",
           );
-        let o = e.slice(0, 3).map((i) =>
-            m.createElement(bt, {
-              key: JSON.stringify(i),
-              value: i,
-              nested: !0,
-              callsById: r,
-            }),
-          ),
+        let o = e
+            .slice(0, 3)
+            .map((i) =>
+              m.createElement(bt, {
+                key: JSON.stringify(i),
+                value: i,
+                nested: !0,
+                callsById: r,
+              }),
+            ),
           a = Dd(o, m.createElement("span", null, ", "));
         return e.length <= 3
           ? m.createElement("span", { style: { color: n.base } }, "[", a, "]")
